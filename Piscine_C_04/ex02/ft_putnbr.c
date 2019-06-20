4fd2a56a45c97fe23a70a2e835e8f80d34ff4045
Thibaut BERNARD <tbernard@e2r10p5.42.fr>
2019-06-20 22:10:00 +0200

    c04

---
+++ Piscine_C_04/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/20 20:58:27 by tbernard          #+#    #+#             */
+/*   Updated: 2019/06/20 22:09:18 by tbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{	
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb > 0 && nb < 10)
+	{
+		ft_putchar(nb + 48); 
+	}
+	else if(nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * (-1));
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
