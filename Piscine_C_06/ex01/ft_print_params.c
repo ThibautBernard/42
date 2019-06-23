ff2c8885ed23a34765c4673e3c853946f153c620
Thibaut BERNARD <tbernard@e2r9p4.42.fr>
2019-06-23 23:14:01 +0200

    c06F

---
+++ Piscine_C_06/ex01/ft_print_params.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/23 18:23:03 by tbernard          #+#    #+#             */
+/*   Updated: 2019/06/23 21:11:24 by tbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+
+}
+void ft_putstr(char *str)
+{
+    int i; 
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+       
+    i = 1;
+    while(i < argc) 
+    {       
+      ft_putstr(argv[i]);
+      ft_putchar('\n');     
+      i++;  
+    }
+    return (0);
+}
---
+++ Piscine_C_06/ex02/ft_rev_params.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/23 21:11:45 by tbernard          #+#    #+#             */
+/*   Updated: 2019/06/23 23:05:16 by tbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+
+}
+
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	
+	int i;
+	int j;	
+	
+	i = 1;
+	while(i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');	
+		i++;
+	}
+
+}
