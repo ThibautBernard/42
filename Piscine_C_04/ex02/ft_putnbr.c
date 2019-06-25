9d07636ae9f6a35d4c4c3474fa89f1b1855276af
Thibaut BERNARD <tbernard@e1r12p7.42.fr>
2019-06-25 16:42:09 +0200

    c042try

--- Piscine_C_04/ex02/ft_putnbr.c
+++ Piscine_C_04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: tbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 20:58:27 by tbernard          #+#    #+#             */
-/*   Updated: 2019/06/23 20:09:08 by tbernard         ###   ########.fr       */
+/*   Updated: 2019/06/24 23:26:07 by tbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	ft_putnbr(int nb)
 		ft_putchar('2');
 		ft_putnbr(147483648);
 	}
-	else if (nb > 0 && nb < 10)
+	else if (nb >= 0 && nb < 10)
 	{
 		ft_putchar(nb + 48);
 	}
