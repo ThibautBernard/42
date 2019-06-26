9e19cef468a6b8cd7a4457bd1bbfabbf133b3232
Thibaut BERNARD <tbernard@e2r11p12.42.fr>
2019-06-26 17:59:27 +0200

    c06l

--- Piscine_C_06/ex00/ft_print_program_name.c
+++ Piscine_C_06/ex00/ft_print_program_name.c
@@ -6,13 +6,20 @@
 /*   By: tbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 18:15:28 by tbernard          #+#    #+#             */
-/*   Updated: 2019/06/26 09:34:33 by tbernard         ###   ########.fr       */
+/*   Updated: 2019/06/26 11:57:34 by tbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int		main(int argc, char **argv)
 {
+	(void)argc;
 	write(1, argv[0], 7);
+	ft_putchar('\n');
 }
